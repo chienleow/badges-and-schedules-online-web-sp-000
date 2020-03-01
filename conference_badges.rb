9a9e61fc0631be5a02749867c8b374004216ec70
C. Leow <chienleow1@gmail.com>
2020-03-01 17:52:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end
